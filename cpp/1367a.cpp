53f0d8be2413a88740c00461729254cf875f47d9
Ali Rido <mohalirido@gmail.com>
2021-02-24 15:28:29 +0700

    Add 1367a.cpp B1490.java

---
+++ cpp/1367a.cpp
@@ -0,0 +1,29 @@
+/*	
+	A. Short Substrings
+	Time limit 2 s
+	Memory Limit 256 MB
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		string s;
+		cin >> s;
+		int length = s.length();
+		if (length == 2) {
+			cout << s << endl;
+		} else {
+			for (int i=0; i<length; i+=2) {
+				printf("%c", s[i]);
+			}
+			printf("%c\n", s[length-1]);
+		}
+	}
+
+	return 0;
+}
