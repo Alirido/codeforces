c23cfe874a3b4cfd5ad2e954b7f430e959d0435b
Ali Rido <mohalirido@gmail.com>
2021-03-03 10:39:07 +0700

    Add 1370a.cpp

---
+++ codeforces/cpp/1370a.cpp
@@ -0,0 +1,20 @@
+/*	
+	A. Maximum GCD
+	Time limit 1 s
+	Memory Limit 256 MB
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		int n;
+		scanf("%d", &n);
+		printf("%d\n", n/2);
+	}
+
+	return 0;
+}
