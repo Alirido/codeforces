c63ab1a5082e5210f0c5dffb9b45c794261ca37c
Ali Rido <mohalirido@gmail.com>
2021-03-03 15:57:01 +0700

    Add 1371a.cpp

---
+++ codeforces/cpp/1371a.cpp
@@ -0,0 +1,20 @@
+/*	
+	A. Magical Sticks
+	Time limit 1 s
+	Memory Limit 256 MB
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		int n;
+		scanf("%d", &n);
+		printf("%d\n", (n+1)/2);
+	}
+
+	return 0;
+}
