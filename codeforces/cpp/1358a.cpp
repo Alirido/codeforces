87b31d8bb89c527600725477cb5d4073a82a5078
Ali Rido <mohalirido@gmail.com>
2021-03-04 15:34:58 +0700

    Add 1358a.cpp

---
+++ codeforces/cpp/1358a.cpp
@@ -0,0 +1,20 @@
+/*	
+	A. Park Lighting
+	Time limit 2 s
+	Memory Limit 256 MB
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		int n, m;
+		scanf("%d %d", &n, &m);
+		printf("%d\n", (n*m+1)/2);
+	}
+
+	return 0;
+}
