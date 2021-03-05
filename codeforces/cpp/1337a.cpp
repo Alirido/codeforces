3fb73a223eb4250ba08795b19eb5f9647eb37c74
Ali Rido <mohalirido@gmail.com>
2021-03-05 17:13:29 +0700

    Add quicksort algorithm

---
+++ codeforces/cpp/1337a.cpp
@@ -0,0 +1,20 @@
+/*	
+	A. Ichihime and Triangle
+	Time limit 1 s
+	Memory Limit 256 MB
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		int a, b, c, d;
+		scanf("%d %d %d %d", &a, &b, &c, &d);
+
+	}
+
+	return 0;
+}
