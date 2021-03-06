667ead428fa8d11c8610bef1f373ce66f3104878
Ali Rido <mohalirido@gmail.com>
2021-03-06 07:21:22 +0700

    Add 1372a.cpp

---
+++ codeforces/cpp/1372a.cpp
@@ -0,0 +1,23 @@
+/*	
+	A, Omkar and Completion
+	Time limit 1 s
+	Memory Limit 256 MB
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		int n;
+		scanf("%d", &n);
+		for (int i=0; i<n; i++) {
+			printf("%d ", 1);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
