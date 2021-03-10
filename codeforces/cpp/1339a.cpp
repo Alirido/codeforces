d9c8c3e486f1eb6bbd3c5af496c364257ef06232
Ali Rido <mohalirido@gmail.com>
2021-03-10 08:13:38 +0700

    Add 1339a.cpp

---
+++ codeforces/cpp/1339a.cpp
@@ -0,0 +1,22 @@
+/*	
+	A. Filling Diamonds
+	Time limit 1 s
+	Memory Limit 256 MB
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		int n;
+		scanf("%d", &n);
+		printf("%d\n", n);
+	}
+
+	return 0;
+}
