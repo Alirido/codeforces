22fad206c76502cc9f25c05a3af80fb71ab8fdc7
Ali Rido <mohalirido@gmail.com>
2021-03-12 23:05:29 +0700

    Add A1285.java

---
+++ codeforces/java/A1285.java
@@ -0,0 +1,17 @@
+/*
+	A. Mezo Playing Zoma
+	Time limit 1 s
+	Memory limit 256 Mb
+*/
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class A1285 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		String s = scan.next();
+		System.out.println(n+1);
+	}
+}
