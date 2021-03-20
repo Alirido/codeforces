3d15ce43fdbd9e7b578a8d728726274ca61576dd
Ali Rido <mohalirido@gmail.com>
2021-03-20 22:37:20 +0700

    Add hackerrank/10 Days of Javascript Day4: classes

---
+++ hackerrank/10 Days of Javascript/classes.js
@@ -0,0 +1,19 @@
+class Polygon {
+	constructor(arr) {
+		this.sides = arr;
+	}
+
+	perimeter() {
+		let result = 0;
+		this.sides.forEach(s => result += s);
+		return result;
+	}
+}
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
