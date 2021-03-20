c767ff1ba08e2a22126a2f517ae0feeef799c74c
Ali Rido <mohalirido@gmail.com>
2021-03-20 12:10:37 +0700

    Add 1391a.cpp

---
+++ codeforces/cpp/1391a.cpp
@@ -0,0 +1,22 @@
+/*	
+	A. Suborrays
+	Time limit 1 s
+	Memory Limit 256 MB
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		int n;
+		scanf("%d", &n);
+		for (int i=1; i<=n; i++)
+			printf("%d ", i);
+		printf("\n");
+	}
+
+	return 0;
+}
