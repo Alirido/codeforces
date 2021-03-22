3f3dc19f7ea58ed9fe1cc5a0a5774cf62005d1f9
Ali Rido <mohalirido@gmail.com>
2021-03-22 20:52:44 +0700

    Add hackerrank/10 Days of Javascript Day6: Bitwise operators

---
+++ hackerrank/10 Days of Javascript/bitwise_operators.js
@@ -0,0 +1,46 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getMaxLessThanK(n, k) {
+    let max_value = 0;
+    for (let i=1; i<n; i++) {
+        for (let j=i+1; j<=n; j++) {
+            let temp = i & j;
+            if (temp < k && temp > max_value)
+                max_value = temp; 
+        }
+    }
+    return max_value;
+}
+
+
+function main() {
+    const q = +(readLine());
+    
+    for (let i = 0; i < q; i++) {
+        const [n, k] = readLine().split(' ').map(Number);
+        
+        console.log(getMaxLessThanK(n, k));
+    }
+}
