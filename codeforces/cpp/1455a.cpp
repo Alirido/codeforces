d0fb3f918d95ba13be89d38a44016be45a6fc835
Ali Rido <mohalirido@gmail.com>
2021-03-27 17:52:19 +0700

    Add 1455a.cpp

---
+++ codeforces/cpp/1455a.cpp
@@ -0,0 +1,21 @@
+/*	
+	A. Strange Function
+	Time limit 2 s
+	Memory Limit 256 MB
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		string n;
+		cin >> n;
+		int ln = n.length();
+		printf("%d\n", ln);
+	}
+
+	return 0;
+}
