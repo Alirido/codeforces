ff3bdc24ba6291bce233de965efb8fe4ec3fa330
Ali Rido <mohalirido@gmail.com>
2021-04-12 06:06:43 +0700

    Add 1393a.cpp

---
+++ codeforces/cpp/1393a.cpp
@@ -0,0 +1,24 @@
+/*	
+	A. Rainbow Dash, Fluttershy and Chess Coloring
+	Time limit 1 s
+	Memory Limit 256 MB
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+typedef long long ll;
+
+int main() {
+	fast_io;
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		cout << n/2 + 1 << endl;
+	}
+
+	return 0;
+}
