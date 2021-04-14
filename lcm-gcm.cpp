411c610e7bde1b1bc7efe0c51e2844805d8fcfcf
Ali Rido <mohalirido@gmail.com>
2021-04-14 07:17:28 +0700

    Add lcm-gcm.cpp

---
+++ lcm-gcm.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+typedef long long ll;
+
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	return gcd(b, a%b);
+}
+
+ll lcm(ll a, ll b) {
+	return a * b / gcd(a, b);
+}
